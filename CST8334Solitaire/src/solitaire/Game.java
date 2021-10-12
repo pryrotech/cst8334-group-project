3d078c091289f3d91bb94394357360addf80aab1
colinash <ash00024@algonquinlive.com>
2021-10-12 11:17:05 -0400

    Deck can now be shuffled! switched to printf (forgot how to format)

--- CST8334Solitaire/src/solitaire/Game.java
+++ CST8334Solitaire/src/solitaire/Game.java
@@ -11,9 +11,9 @@ public class Game {
 		//start game
 		Deck d1 = new Deck();
 		d1.populate();
-		System.out.println("This is a new deck" +  "/n" + d1.showDeck());
+		System.out.printf("This is a new deck" +  "%n" + d1.showDeck());
 		d1.shuffle();
-		System.out.println("The deck has now been shuffled" + "/n" + d1.showDeck());
+		System.out.printf("The deck has now been shuffled" + "%n" + d1.showDeck());
 
 
 		Tableau newGame = new Tableau();
